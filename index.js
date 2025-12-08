38631c0829f8afcd033dac6f0c9cfc73f4b3855e
obaidullah-miazi-dev <obaidullahmiazi.dev@gmail.com>
2025-12-08 20:53:56 +0600

    added specific club data get api

--- index.js
+++ index.js
@@ -74,6 +74,13 @@ async function run() {
     res.send(result)
   })
 
+  app.get('/clubs/:id',async(req,res)=>{
+    const id = req.params.id 
+    const query = {_id: new ObjectId(id)}
+    const result = await clubsCollection.find(query).toArray()
+    res.send(result)
+  })
+
 
     // club manager related apis 
     app.post('/clubManager',async(req,res)=>{
