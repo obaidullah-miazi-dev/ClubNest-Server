2fbdc28239234efd45910da08b5ab4426ee70089
obaidullah-miazi-dev <obaidullahmiazi.dev@gmail.com>
2025-12-14 13:12:26 +0600

    added specific event data get api

--- index.js
+++ index.js
@@ -416,6 +416,13 @@ async function run() {
       res.send(result)
     })
 
+    app.get('/getEvent/:id',async(req,res)=>{
+      const id = req.params.id 
+      const query = {_id: new ObjectId(id)}
+      const result = await eventsCollection.find(query).toArray()
+      res.send(result)
+    })
+
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     console.log(
