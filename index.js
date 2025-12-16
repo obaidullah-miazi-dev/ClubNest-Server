aa10163021b5f8715b1b4285b52f11eed0f5d48b
obaidullah-miazi-dev <obaidullahmiazi.dev@gmail.com>
2025-12-16 19:16:34 +0600

    fix firebase env for vercel

--- index.js
+++ index.js
@@ -808,6 +808,6 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`);
+// });
